fb3a2df81cc7116aaa628abf7e7f8bc9732a0039
Lê Ngọc Phúc <phuc97405@gmail.com>
2024-06-21 08:57:03 +0700

    hi

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3644107107"
+    serviceWorkerVersion: "2599728663"
   }
 });
